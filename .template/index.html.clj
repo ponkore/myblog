5056dee2cb2852959b9524007be0d3f7713b1595
Ponkore <ponkore@me.com>
2012-11-29 22:33:08 +0900

    initial commit

---
+++ .template/index.html.clj
@@ -0,0 +1,6 @@
+;; Define template options here
+; @layout  default
+; @title   home
+
+[:article
+ (my-post-list site :with-summary true)]
---
+++ .template/layouts/tag.clj
@@ -0,0 +1,6 @@
+;; 
+; @layout  default
+; @title   tag
+
+[:article
+ (my-post-list site)]
