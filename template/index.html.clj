b514b1eeb45fc73b94c7bcafac13abe267a44bec
Ponkore <ponkore@me.com>
2013-03-15 00:13:42 +0900

    better support for pagination button.

--- template/index.html.clj
+++ template/index.html.clj
@@ -4,8 +4,12 @@
 
 (my-post-list site)
 
-(if-let [url (:prev-page site)]
-  (link "previous page" url))
-
-(if-let [url (:next-page site)]
-  (link "next page" url))
+[:nav {:class "ink-navigation"}
+ [:ul {:class "pagination"}
+  (if-let [prev-url (:prev-page site)]
+    [:li {:class "previous"} (link "Previous" prev-url)]
+    [:li {:class "disabled previous"} (link "Previous" "#")])
+  (if-let [next-url (:next-page site)]
+    [:li {:class "next"} (link "Next" next-url)]
+    [:li {:class "disabled next"} (link "Next" "#")])
+  ]]
