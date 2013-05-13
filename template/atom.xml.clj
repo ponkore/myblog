30a969fe2bd05b2be84b8960dab5defe7cec2701
Ponkore <ponkore@me.com>
2013-05-14 00:52:59 +0900

    atom feed format modified. 2013/5/12 post file modified (table css class, etc.)

--- template/atom.xml.clj
+++ template/atom.xml.clj
@@ -1,18 +1,19 @@
 (str "<?xml version=\"1.0\" encoding=\"" (:charset site) "\"?>")
 [:feed {:xmlns "http://www.w3.org/2005/Atom"}
- [:title (:title site)]
+ [:title (:site-title site)]
  [:link    {:href (str (:atom-base site) "/atom.xml"), :rel "self"}]
  [:link    {:href (:atom-base site)}]
- [:updated (date->xml-schema (:date site))]
+ [:updated (date->xml-schema (clj-time.core/now))]
  [:id      (:atom-base site)]
  [:author  [:name (str "@" (:twitter site))]]
 
  (for [post (:posts site)]
    [:entry
-    [:title   (:title post)]
-    [:link    (str (:atom-base site) (:url post))]
-    [:updated (date->xml-schema (:date post))]
-    [:id      (str (:atom-base site) (:url post))]
+    [:title     (:title post)]
+    [:link      {:href (str (:atom-base site) (:url post))}]
+    [:id        (str (:atom-base site) (:url post))]
+    [:published (date->xml-schema (:date post))]
+    [:updated   (date->xml-schema (:date post))]
     [:content {:type "html"}
      (force (:lazy-content post))]])]
 
