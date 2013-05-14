215f2c1dfb82d84956a8d64af13c82fd7c79677f
ponkore <ponkore@gmail.com>
2013-05-14 12:18:55 +0900

    atom format modified.

--- template/atom.xml.clj
+++ template/atom.xml.clj
@@ -1,7 +1,8 @@
 (str "<?xml version=\"1.0\" encoding=\"" (:charset site) "\"?>")
 [:feed {:xmlns "http://www.w3.org/2005/Atom"}
  [:title (:site-title site)]
- [:link    {:href (str (:atom-base site) "/atom.xml"), :rel "self"}]
+ [:link    {:href (str (:atom-base site) "/atom.xml")
+            :type "text/html" :rel "alternate" :title (:site-title site)}]
  [:link    {:href (:atom-base site)}]
  [:updated (date->xml-schema (clj-time.core/now))]
  [:id      (:atom-base site)]
@@ -10,9 +11,10 @@
  (for [post (:posts site)]
    [:entry
     [:title     (:title post)]
-    [:link      {:href (str (:atom-base site) (:url post))}]
+    [:link      {:href (str (:atom-base site) (:url post))
+                 :type "text/html" :rel "alternate" :title (:title post)}]
     [:id        (str (:atom-base site) (:url post))]
-    [:published (date->xml-schema (:date post))]
+    [:issued    (date->xml-schema (:date post))]
     [:updated   (date->xml-schema (:date post))]
     [:content {:type "html"}
      (force (:lazy-content post))]])]
