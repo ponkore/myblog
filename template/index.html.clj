31f8bad7ea5077b792ac18040f719a2f5064f237
Ponkore <ponkore@me.com>
2014-01-08 00:12:00 +0900

    modify index page's link (Previous/Next -> Newer/Older)

--- template/index.html.clj
+++ template/index.html.clj
@@ -7,9 +7,9 @@
 [:nav {:class "ink-navigation"}
  [:ul {:class "pagination"}
   (if-let [prev-url (:prev-page site)]
-    [:li {:class "previous"} (link "Previous" prev-url)]
-    [:li {:class "disabled previous"} (link "Previous" "#")])
+    [:li {:class "previous"} (link "Newer" prev-url)]
+    [:li {:class "disabled previous"} (link "Newer" "#")])
   (if-let [next-url (:next-page site)]
-    [:li {:class "next"} (link "Next" next-url)]
-    [:li {:class "disabled next"} (link "Next" "#")])
+    [:li {:class "next"} (link "Older" next-url)]
+    [:li {:class "disabled next"} (link "Older" "#")])
   ]]
